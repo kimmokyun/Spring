3b7bd00f67029cd4027b828f99e508aae250bd82
kimmokyun <kaqq77@gmail.com>
2022-11-18 21:02:57 +0900

    "2022"

---
+++ spring_05_jdbc/src/main/java/part01_mybatis/Memcontroller.java
@@ -0,0 +1,59 @@
+package part01_mybatis;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+//http://localhost:8090/myapp/list.do
+@Controller
+public class Memcontroller {
+    @Autowired
+	private MemDAO memDAO;
+	
+	public Memcontroller() {
+		// TODO Auto-generated constructor stub
+	}
+ public void setMemDAO(MemDAO memDAO) {
+	 this.memDAO = memDAO;
+ }
+ 
+ @RequestMapping(value="list.do")
+	public ModelAndView process(ModelAndView mav) {
+		mav.addObject("list", memDAO.list());
+		mav.setViewName("part01/list");
+		return mav;
+	}
+ @RequestMapping(value="/insert.do", method=RequestMethod.GET)
+	public String insertForm() {
+		return "part01/insert";
+}
+ @RequestMapping(value="/insert.do", method=RequestMethod.POST)
+	public String insertData(MemDTO dto) {
+	 memDAO.insertMethod(dto);
+		return "redirect:/list.do";
+}
+ @RequestMapping(value="/update.do", method=RequestMethod.GET)
+ public ModelAndView updateForm(int num, ModelAndView mav) {
+	 mav.addObject("dto", memDAO.one(num));
+	 mav.setViewName("part01/update");
+	 return mav;
+	 
+ }
+ @RequestMapping(value="/update.do", method=RequestMethod.POST)
+ public String updateDate(MemDTO dto) {
+	 memDAO.updateMethod(dto);
+	 return "redirect:/list.do";
+ }
+ @RequestMapping(value="/delete.do")
+ public String deleteData(int num) {
+	 memDAO.deleteMethod(num);
+	 return "redirect:/list.do";
+ }
+ 
+ 
+ 
+ 
+ 
+ 
+}
